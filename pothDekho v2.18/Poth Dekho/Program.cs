8f790b326a3d8375b30542f716d9c07d7d768579
Mushfiq97 <94002445+Mushfiq97@users.noreply.github.com>
2021-12-18 23:15:15 +0600

    final for admin

---
+++ pothDekho v2.18/Poth Dekho/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poth_Dekho
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //Application.Run(new tourpackage());
+            //Application.Run(new Choose_package(1));
+            //Application.Run(new booked_trip(1));
+            //Application.Run(new admin_booked_packages("admin1"));
+            //Application.Run(new admin_dashboard("admin1"));
+            //Application.Run(new admin_add_traveller("admin1"));
+            //Application.Run(new traveller_create_account());
+            //Application.Run(new admin_add_guide("admin1"));
+            //Application.Run(new Admin_Reg());
+            //Application.Run(new admin_view_profile("aqib1"));
+            Application.Run(new admin_login());
+
+        }
+    }
+}
---
+++ pothDekho v2.18/Poth Dekho/admin_dashboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poth_Dekho
+{
+    public partial class admin_dashboard : Form
+    {
+        string username="";
+        public admin_dashboard(string user)
+        {
+            InitializeComponent();
+            this.username = user;
+        }
+
+        private void btnPackages_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            admin_tourpackage f = new admin_tourpackage(username);
+            f.Show();
+        }
+
+        private void btnTraveller_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            admin_traveller_information f = new admin_traveller_information(username);
+            f.Show();
+        }
+
+        private void btnBookedPackages_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            admin_booked_packages f = new admin_booked_packages(username);
+            f.Show();
+        }
+
+        private void btnViewProfile_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            admin_view_profile f = new admin_view_profile(username);
+            f.Show();
+        }
+    }
+}
